a799066cc6b7e3d0d0257d42a79ce72f92e8e64e
Vikash Sharma <1672153@tcs.com>
2021-09-21 17:47:27 +0000

    Code

--- src/App.js
+++ src/App.js
@@ -9,10 +9,6 @@ import Login from "./components/login.component";
 import Register from "./components/register.component";
 import Home from "./components/home.component";
 import Profile from "./components/profile.component";
-import BoardUser from "./components/board-user.component";
-import BoardModerator from "./components/board-moderator.component";
-import BoardAdmin from "./components/board-admin.component";
-
 // import AuthVerify from "./common/auth-verify";
 import EventBus from "./common/EventBus";
 
@@ -22,9 +18,7 @@ class App extends Component {
     this.logOut = this.logOut.bind(this);
 
     this.state = {
-      showModeratorBoard: false,
-      showAdminBoard: false,
-      currentUser: undefined,
+      currentUser: undefined
     };
   }
 
@@ -33,9 +27,7 @@ class App extends Component {
 
     if (user) {
       this.setState({
-        currentUser: user,
-        showModeratorBoard: user.roles.includes("ROLE_MODERATOR"),
-        showAdminBoard: user.roles.includes("ROLE_ADMIN"),
+        currentUser: user
       });
     }
     
@@ -58,36 +50,21 @@ class App extends Component {
   }
 
   render() {
-    const { currentUser, showModeratorBoard, showAdminBoard } = this.state;
+    const { currentUser} = this.state;
 
     return (
-      <div>
-        <nav className="navbar navbar-expand navbar-dark bg-dark">
-          <Link to={"/"} className="navbar-brand">
-            bezKoder
+      <div className="container">
+        <nav className="navbar navbar-expand navbar-dark bg-light ">
+          <Link to={"/"} className="navbar-brand text-dark">
+            FavAvtar
           </Link>
           <div className="navbar-nav mr-auto">
-            <li className="nav-item">
-              <Link to={"/home"} className="nav-link">
+            <li className="nav-item text-dark">
+              <Link to={"/home"} className="nav-link text-dark">
                 Home
               </Link>
             </li>
-
-            {showModeratorBoard && (
-              <li className="nav-item">
-                <Link to={"/mod"} className="nav-link">
-                  Moderator Board
-                </Link>
-              </li>
-            )}
-
-            {showAdminBoard && (
-              <li className="nav-item">
-                <Link to={"/admin"} className="nav-link">
-                  Admin Board
-                </Link>
-              </li>
-            )}
+ 
 
             {currentUser && (
               <li className="nav-item">
@@ -101,42 +78,39 @@ class App extends Component {
           {currentUser ? (
             <div className="navbar-nav ml-auto">
               <li className="nav-item">
-                <Link to={"/profile"} className="nav-link">
+                <Link to={"/profile"} className="nav-link text-dark">
                   {currentUser.username}
                 </Link>
               </li>
               <li className="nav-item">
-                <a href="/login" className="nav-link" onClick={this.logOut}>
-                  LogOut
+                <a href="/login" className="nav-link text-dark" onClick={this.logOut}>
+                  LogOut Wallet
                 </a>
               </li>
             </div>
           ) : (
             <div className="navbar-nav ml-auto">
               <li className="nav-item">
-                <Link to={"/login"} className="nav-link">
-                  Login
+                <Link to={"/login"} className="nav-link text-dark">
+                  Login Wallet
                 </Link>
               </li>
 
               <li className="nav-item">
-                <Link to={"/register"} className="nav-link">
-                  Sign Up
+                <Link to={"/register"} className="nav-link text-dark">
+                Register Wallet
                 </Link>
               </li>
             </div>
           )}
         </nav>
 
-        <div className="container mt-3">
+        <div className="container">
           <Switch>
             <Route exact path={["/", "/home"]} component={Home} />
             <Route exact path="/login" component={Login} />
             <Route exact path="/register" component={Register} />
             <Route exact path="/profile" component={Profile} />
-            <Route path="/user" component={BoardUser} />
-            <Route path="/mod" component={BoardModerator} />
-            <Route path="/admin" component={BoardAdmin} />
           </Switch>
         </div>
 
--- src/components/home.component.js
+++ src/components/home.component.js
@@ -1,40 +1,29 @@
 import React, { Component } from "react";
 
-import UserService from "../services/user.service";
+// import UserService from "../services/user.service";
 
 export default class Home extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      content: ""
+      content: "Favtar Wallet."
     };
   }
 
   componentDidMount() {
-    UserService.getPublicContent().then(
-      response => {
-        this.setState({
-          content: response.data
-        });
-      },
-      error => {
-        this.setState({
-          content:
-            (error.response && error.response.data) ||
-            error.message ||
-            error.toString()
-        });
-      }
-    );
+
   }
 
   render() {
     return (
-      <div className="container">
+      <div className="container" style={{ marginTop: "20px" }}>
         <header className="jumbotron">
-          <h3>{this.state.content}</h3>
+          <h4>{this.state.content}</h4>
         </header>
+        <div className="container-md" style={{ marginTop: "20px" }}>
+          <p>{"Favtar Wallet Favtar Wallet Favtar Wallet Favtar Wallet"}</p>
+        </div>
       </div>
     );
   }
